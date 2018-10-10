44e7c4f26a579f65cd6788978d71e12f7a922dce
Vince Andrew Pimentel <contact@vincep.me>
2018-10-10 23:47:15 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,10 +15,6 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase.downcase
-    "HUH?! SPEAK UP, SONNY!"
-  elsif phrase.upcase == true
-    "NO, NOT SINCE 1938!"
   end
 
 
